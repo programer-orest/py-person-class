a535f23e44676676f11eb41730d1b0ceaa386082
Orest Yakymiv <akimivorest@gmail.com>
2025-03-20 18:47:04 +0200

    create method create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list) -> list[Person]:
+    person_list = []
+    for person in people:
+        p = Person(person["name"], person["age"])
+        person_list.append(p)
+
+    for person in people:
+        p = Person.people.get(person["name"])
+        if person.get("wife"):
+            p.wife = Person.people.get(person["wife"])
+        if person.get("husband"):
+            p.husband = Person.people.get(person["husband"])
+
+    return person_list
